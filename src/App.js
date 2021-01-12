0c9f47fcccc07644393748aab59c6371fd71a7a9
Rebecca Still <beckystill@hotmail.com>
2021-01-12 21:08:26 +0000

    test responsive iframe

--- src/App.js
+++ src/App.js
@@ -2,7 +2,10 @@ import "./App.css";
 
 function App() {
   return (
-    <></>
+    <iframe
+      class="responsive-iframe"
+      src="https://advent.aws.scottishtecharmy.org/"
+    ></iframe>
   );
 }
 
