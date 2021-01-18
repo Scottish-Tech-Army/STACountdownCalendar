96a0e11eea28a97df8afb412629a4ff25f80c7d0
Rebecca Still <beckystill@hotmail.com>
2021-01-18 13:41:09 +0000

    additional fixed for responsive views with fixed header

--- src/App.js
+++ src/App.js
@@ -7,7 +7,6 @@ function App() {
   return (
     <div className="app">
       <Header />
-      <hr />
       <Main />
     </div>
   );
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,8 +3,11 @@ import "./Header.css";
 function Header() {
   return (
     <div className="header-container">
-      <img src="assets/Dunbar.png" alt="logo"/>
-      <h1>Dunbar Promotion Calendar</h1>
+      <div className="header-content">
+        <img src="assets/Dunbar.png" alt="logo" />
+        <h1>Dunbar Promotion Calendar</h1>
+      </div>
+      <hr />
     </div>
   );
 }
