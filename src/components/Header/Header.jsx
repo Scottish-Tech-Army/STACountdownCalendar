30e701e6497fdfd2737f439bcb2f2ea8c3952ca0
Rebecca Still <beckystill@hotmail.com>
2021-02-11 18:38:29 +0000

    add content to header and SOLE font

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,9 +3,12 @@ import "./Header.css";
 function Header() {
   return (
     <div className="header-contents">
-      <div>
+
         <img src="assets/DunbarLogo.png" alt="Dunbar town logo" />
-      </div>
+        <div className="header-text">
+          <p>28 DAYS, 28 AMAZING OFFERS.  Support your local economy and claim your daily offer.  Visit the promotion calendar each day to claim another great offer<span>&#42;</span>.</p>
+          <p><span>&#42;</span>explain how offers can only be claimed through the SOLE platform?</p>
+        </div>
     </div>
   );
 }
