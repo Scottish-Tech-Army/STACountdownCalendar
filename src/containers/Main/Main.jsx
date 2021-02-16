5705c7329019deb04b8923e92d4355c792ea11a9
Calum_Muir <mulacrium@gmail.com>
2021-02-16 09:14:38 +0000

    Implementing autoscroll to current day - Calum

--- src/containers/Main/Main.jsx
+++ src/containers/Main/Main.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./Main.css";
 import moment from "moment";
 import Container from "react-bootstrap/Container";
@@ -8,7 +8,10 @@ import PromotionWindow from "../../components/PromotionWindow/PromotionWindow";
 import PromotionModal from "../../components/PromotionModal/PromotionModal";
 import config from "../../config.json";
 
+
 function Main() {
+  window.history.scrollRestoration = "manual";      //I added this to stop browser recalling last scroll location - Calum.
+ 
   const allDays = config.days;
 
   // For Production
@@ -27,8 +30,11 @@ function Main() {
 
   const promotionWindows = allDays.map((promotionWindow, index) => {
     return (
+      <>
+
       <Button
         key={index}
+        id={promotionWindow.Date}
         onClick={handleShow}
         className={(promotionWindow.Date !== currentDate) ? "promotion-window-button" : "promotion-window-button-current-date"}
       >
@@ -37,14 +43,23 @@ function Main() {
           promotionWindowDate={moment(promotionWindow.Date)}
         />
       </Button>
+      </>
     );
   });
 
+  useEffect(() => {        
+    if (window.matchMedia('(max-width: 600px)').matches) {                     //If mobile/vertical view is in effect                
+    document.getElementById(currentDate).scrollIntoView();                     //Scrolls to the current day.
+    }
+  })
+
   return (
-    <Container fluid className="main-container">
+    <>
+      <Container fluid className="main-container">
       <Row className="promotion-window-container">{promotionWindows}</Row>
       <PromotionModal show={show} handleClose={handleClose} />
     </Container>
+    </>
   );
 }
 
