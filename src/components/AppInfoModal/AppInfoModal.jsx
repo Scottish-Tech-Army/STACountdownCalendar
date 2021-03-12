6b126af5c33404da88ff717db17adfb08e26a622
Calum_Muir <mulacrium@gmail.com>
2021-03-12 09:09:58 +0000

    Added close button to placeholder modal

--- src/components/AppInfoModal/AppInfoModal.jsx
+++ src/components/AppInfoModal/AppInfoModal.jsx
@@ -1,9 +1,15 @@
 import './AppInfoModal.css';
 
 export default function AppInfoModal() {
+  //handle closing of the "modal"
+  function closeAppInfoModal() {
+    document.querySelector('.app-info-modal').classList.add('hidden');
+  }
+
   return (
     <div className="app-info-modal">
       <h1>APP INFO GOES HERE!</h1>
+      <button onClick={closeAppInfoModal}>OK</button>
     </div>
   );
 }
