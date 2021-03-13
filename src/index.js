3813ab1abcb4b53eb15d0c3388e601b8c30832b8
Becky Still <b3ckystill@gmail.com>
2021-03-13 11:04:02 +0000

    Fix issue with font not loading in Safari and Firefox

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import './fonts/Granville-Regular.otf'
+import './fonts/GranvilleRegular.otf'
 
 ReactDOM.render(
   <React.StrictMode>
